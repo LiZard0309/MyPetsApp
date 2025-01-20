b5433f34af609d3617f7576a08083594eb025dfc
LiZard0309 <lisahofbauer3992@gmail.com>
2025-01-20 17:06:02 +0100

    first commit

---
+++ MyPetsApp/MyPetsApp/MyPetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPetsAppApp.swift
+//  MyPetsApp
+//
+//  Created by user on 20.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MyPetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
